17d59d896864c805e08d1b1a300db417a94092a3
MoY8462 <moy.sav21@hotmail.com>
2022-12-14 11:35:41 -0600

    build: :tada: practica 9

--- app/src/main/java/dgtic/unam/jsonxml/MainActivity.kt
+++ app/src/main/java/dgtic/unam/jsonxml/MainActivity.kt
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.Response.error
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.StringRequest
 import dgtic.unam.jsonxml.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONObject
 import java.net.URLEncoder
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -26,6 +33,20 @@ class MainActivity : AppCompatActivity() {
             )
             buscar()
         }
+
+        binding.restxml.setOnClickListener {
+            studenXML()
+        }
+
+        binding.restjson.setOnClickListener{
+            studenJSON()
+        }
+        binding.restjsonid.setOnClickListener{
+            studiantID()
+        }
+        binding.restjsonadd.setOnClickListener{
+            studentAdd()
+        }
     }
 
     private fun buscar(){
@@ -41,5 +62,159 @@ class MainActivity : AppCompatActivity() {
         }
         volleyAPI.add(stringRequest)
     }
+
+    private fun studenXML(){
+        val urlXML = "http://"+ipPuerto+"/estudiantesXML"
+        val stringRequest=object: StringRequest(
+            Request.Method.GET,urlXML,Response.Listener<String>{response->
+                binding.outText.text=response
+            },Response.ErrorListener { binding.outText.text = "No se encuentra la informacion, revice si conexion" }){
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers=HashMap<String,String>()
+                headers["User-Agent"]="Mozilla/5.0 (Windows NT 6.1)"
+                return headers
+            }
+        }
+        volleyAPI.add(stringRequest)
+    }
+
+    private fun studenJSON(){
+        val urlJSON = "http://"+ipPuerto+"/estudiantesJSON"
+        var cadena = ""
+        val jsonRequest = object: JsonArrayRequest(
+            urlJSON,
+            Response.Listener<JSONArray>{ response ->
+                (0 until response.length()).forEach{
+                    val estudiante = response.getJSONObject(it)
+                    val materia = estudiante.getJSONArray("materias")
+                    cadena+=estudiante.get("cuenta").toString()+">"
+                    (0 until materia.length()).forEach{
+                        val datos = materia.getJSONObject(it)
+                        cadena += datos.get("nombre").toString()+"**"+datos.get("creditos").toString()+"---"
+                    }
+                    cadena+=">\n"
+                }
+                binding.outText.text = cadena
+            },
+            Response.ErrorListener {
+                binding.outText.text = "No se encuentra la información, revise la conexión"
+            }
+        ){
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers=HashMap<String,String>()
+                headers["User-Agent"]="Mozilla/5.0 (Windows NT 6.1)"
+                return headers
+            }
+        }
+        volleyAPI.add(jsonRequest)
+    }
+
+    private fun studiantID() {
+        val urlJSON = "http://" + ipPuerto + "/id/" + binding.searchText.text.toString()
+        var cadena = ""
+        val jsonRequest = object : JsonObjectRequest(
+            Method.GET,
+            urlJSON,
+            null,
+            Response.Listener<JSONObject> { response ->
+                binding.outText.text = response.get("cuenta")
+                    .toString() + "----" + response.get("nombre").toString() + "\n"
+            },
+            Response.ErrorListener {
+                binding.outText.text = "No se encuentra la información, revice la conexión"
+            }
+        ) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1)"
+                return headers
+            }
+        }
+        volleyAPI.add(jsonRequest)
+    }
+
+    private fun studentAdd(){
+        val urlJSON = "http://"+ipPuerto+"/agregarestudiante"
+        var cadena = ""
+        val jsonRequest = object: JsonArrayRequest(
+            urlJSON,
+            Response.Listener<JSONArray>{ response ->
+                (0 until response.length()).forEach{
+                    val estudiante = response.getJSONObject(it)
+                    val materia = estudiante.getJSONArray("materias")
+                    cadena+=estudiante.get("cuenta").toString()+">"
+                    (0 until materia.length()).forEach{
+                        val datos = materia.getJSONObject(it)
+                        cadena += datos.get("nombre").toString()+"**"+datos.get("creditos").toString()+"---"
+                    }
+                    cadena+=">\n"
+                }
+                binding.outText.text = cadena
+            },
+            Response.ErrorListener {
+                binding.outText.text = "No se encuentra la información, revise la conexión"
+            }
+        ){
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers=HashMap<String,String>()
+                headers["User-Agent"]="Mozilla/5.0 (Windows NT 6.1)"
+                return headers
+            }
+
+            override fun getBody(): ByteArray {
+                var estudiante = JSONObject()
+                estudiante.put("cuenta", "A000")
+                estudiante.put("nombre","Android")
+                estudiante.put("edad","200")
+                var materias = JSONArray()
+                val itemMaterias = JSONObject()
+                itemMaterias.put("id","1")
+                itemMaterias.put("nombre", "Nueva Materia")
+                itemMaterias.put("creditos", "100")
+                materias.put(itemMaterias)
+                estudiante.put("materias",materias)
+                return estudiante.toString().toByteArray(charset = Charsets.UTF_8)
+            }
+
+            override fun getMethod(): Int {
+                return Method.POST
+            }
+        }
+        volleyAPI.add(jsonRequest)
+    }
+    private fun studentsDelete() {
+        val urlJSON = "http://"+ipPuerto+"/borrarestudiante/"+binding.searchText.text.toString()
+        var cadena = ""
+        val jsonRequest = object : JsonArrayRequest(
+            urlJSON,
+            Response.Listener<JSONArray> { response ->
+                (0 until response.length()).forEach {
+                    val estudiante = response.getJSONObject(it)
+                    val materia=estudiante.getJSONArray("materias")
+                    cadena += estudiante.get("cuenta").toString() + "<-"
+                    (0 until materia.length()).forEach {
+                        val datos=materia.getJSONObject(it)
+                        cadena += datos.get("nombre").
+                        toString() + "----" + datos.get("creditos").toString() + "--"
+                    }
+                    cadena+="> \n"
+                }
+                binding.outText.text = cadena
+            },
+            Response.ErrorListener {
+                binding.outText.text = getString(R.string.error)
+            }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1)"
+                return headers
+            }
+            override fun getMethod(): Int {
+                return return Method.DELETE
+            }
+        }
+        volleyAPI.add(jsonRequest)
+    }
+
 
 }
